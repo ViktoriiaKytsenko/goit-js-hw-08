92729474f9df3d4ffa6c4465feef2996f523f993
Viktoriia_Kytsenko <vikakitsenko@gmail.com>
2025-05-09 22:35:04 +0200

    fınısh

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const inputEl = document.getElementById("name-input");
-const outputEl = document.getElementById("name-output");
-
-inputEl.addEventListener("input", () => {
-  const trimmedValue = inputEl.value.trim();
-  outputEl.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
-});
